b8d762b58899cfa2a62dac9aefe54999b084d42b
Obayashi <4210.tatu@gmail.com>
2018-03-30 23:34:18 +0900

    P97-104 テンプレートエンジンを利用する

--- node-app/app.js
+++ node-app/app.js
@@ -1,23 +1,21 @@
 const http = require('http');
 const fs = require('fs');
+const ejs = require('ejs');
+
+const index_page = fs.readFileSync('./index.ejs','utf8');
 
 var server = http.createServer(getFromClient);
 server.listen(3000);
 console.log('Server start!');
 
 //createServerの処理
-function getFromClient(req,res){
-  request = req;
-  response = res;
-    fs.readFile('./index.html','UTF-8',writeToResponse);
-}
-// readFile完了後の処理
-function writeToResponse(error,data){
-var content = data.
-    replace(/dummy_title/g,'タイトルです').
-    replace(/dummy_content/g,'これがコンテンツです');
+function getFromClient(request,response){
 
+  var content = ejs.render(index_page,{
+    title:"Indexページ",
+    content:"これはテンプレートを使ったサンプルページです"
+  });
   response.writeHead(200,{'Content-Type': 'text/html'});
   response.write(content);
-  response.end;
+  response.end();
 }
