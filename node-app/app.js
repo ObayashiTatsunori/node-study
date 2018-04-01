182a71ef52abf0acc6901e3692f36006a2ab6596
Obayashi <4210.tatu@gmail.com>
2018-04-01 20:54:06 +0900

    P105-118 ルーティングをマスター

--- node-app/app.js
+++ node-app/app.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
-
+const url =require('url');
 const index_page = fs.readFileSync('./index.ejs','utf8');
+const style_css = fs.readFileSync('./style.css','utf8');
+const other_page = fs.readFileSync('./other.ejs','utf8');
 
 var server = http.createServer(getFromClient);
 server.listen(3000);
@@ -10,12 +12,36 @@ console.log('Server start!');
 
 //createServerの処理
 function getFromClient(request,response){
+  var url_parts = url.parse(request.url);
+  switch (url_parts.pathname){
+    case '/':
+      var content = ejs.render(index_page,{
+        title:"Indexページ",
+        content:"これはテンプレートを使ったサンプルページです"
+      });
+      response.writeHead(200,{'Content-Type': 'text/html'});
+      response.write(content);
+      response.end();
+      break;
+    case '/style.css':
+      response.writeHead(200,{'Content-Type':'text/css'});
+      response.write(style_css);
+      response.end();
+      break;
+    case '/other':
+      var content = ejs.render(other_page,{
+        title:"Otherページ",
+        content:"これは新しく追加したページです"
+      });
+      response.writeHead(200,{'Content-Type':'text/html'});
+      response.write(content);
+      response.end();
+      break;
+    default:
+      response.writeHead(200,{'Content-Type':'text/html'});
+      response.end('no page...');
+      break;
+  }
 
-  var content = ejs.render(index_page,{
-    title:"Indexページ",
-    content:"これはテンプレートを使ったサンプルページです"
-  });
-  response.writeHead(200,{'Content-Type': 'text/html'});
-  response.write(content);
-  response.end();
+
 }
