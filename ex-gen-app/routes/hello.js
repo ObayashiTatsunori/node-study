d78c48cf6586445b0c272b6c084e988c13d10e71
EC2 Default User <4210.tatu@gmail.com>
2018-04-30 01:58:35 +0000

    P244-254 パラメータとフォーム送信

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -2,11 +2,25 @@ var express = require("express");
 var router = express.Router();
 
 router.get('/',(req,res,next) =>{
+    var msg = '※何か書いてください';
+    if(req.session.message != undefined){
+        msg="Last Message: "+ req.session.message;
+    }
     var data = {
         title : 'Hello!',
-        content : 'これは、サンプルのコンテンツです。<br> this is sample content.'
+        content : msg
     };
     res.render('hello',data);
 });
+router.post('/post',(req,res,next)=>{
+    var msg = req.body['message'];
+    req.session.message=msg;
+    var data = {
+        title : 'Hello!',
+        content : 'Last Message: '+req.session.message
+    }
+    res.render('hello',data);
+})
+
 module.exports=router;
     
