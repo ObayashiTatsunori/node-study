bedcd5c017c8f6dcf1d3ce4e76f24c61f273af1a
Obayashi <4210.tatu@gmail.com>
2018-05-06 23:02:47 +0900

    P271-294 データベースを使おう

--- ex-gen-app/app.js
+++ ex-gen-app/app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session = require("express-session");
-var jquery = require("express-jquery");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hello = require("./routes/hello");
@@ -19,33 +17,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(jquery('/jquery'));
-var ajax = require("./routes/ajax");
-var session_opt={
-  secret:'keyboard cat',
-  resave:false,
-  saveUninitialized:false,
-  cookie:{maxAge:60*60*1000}
-};
-app.use(session(session_opt));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/hello',hello)
-app.use('/ajax',ajax);
-// catch 404 and forward to error handler
+app.use('/hello', hello)
+    // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -1,31 +1,26 @@
 var express = require("express");
 var router = express.Router();
 
-var http = require("https");
-var parseString = require('xml2js').parseString;
+var sqlite3 = require('sqlite3');
 
-router.get('/',(req,res,next) =>{
-    var opt={
-        host:'news.google.com',
-        port:443,
-        path: '/news?hl=ja&ned=us&ie=UTF-8&oe=UTF-8&output=rss'
-    };
-    http.get(opt,(res2)=>{
-        var body ='';
-        res2.on('data',(data)=>{
-            body += data;
-        });
-        res2.on('end',()=>{
-            parseString(body.trim(),(err,result)=>{
-                var data ={
-                    title:'Hello!',
-                    content: result.rss.channel[0].item
+// データベースオブジェクトの取得
+var db = new sqlite3.Database('mydb.sqlite3');
+// GETアクセスの処理
+router.get('/', (req, res, next) => {
+    // データベースのシリアライズ
+    db.serialize(() => {
+        // レコードをすべて取り出す
+        db.all("select * from mydata", (err, rows) => {
+            // データベースアクセス完了時の処理
+            if (!err) {
+                var data = {
+                    title: 'Hello!',
+                    content: rows
                 };
-                res.render('hello',data);
-            });
+                res.render('hello', data);
+            }
         });
     });
 });
 
-module.exports=router;
-    
+module.exports = router;
