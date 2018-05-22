9ca93eb08ec72edc3a88a1c8e3e1bdf9d2ca68c2
Obayashi <4210.tatu@gmail.com>
2018-05-23 07:20:31 +0900

    P381-386 掲示版のホーム画面の処理を追加

--- mini_board_2/app.js
+++ mini_board_2/app.js
@@ -4,9 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var validator = require('express-validator');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var home = require('./routes/home');
 
 var app = express();
 
@@ -21,15 +24,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(validator());
 
-app.use('/', routes);
+var session_opt = {
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 }
+};
+app.use(session(session_opt));
+
+app.use('/', index);
 app.use('/users', users);
+app.use('/home', home);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -37,23 +50,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- mini_board_2/routes/users.js
+++ mini_board_2/routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+var knex = require('knex')({
+    dialect: 'sqlite3',
+    connection: {
+        filename: 'board_data.sqlite3'
+    },
+    useNullAsDefault: true
+});
+
+var Bookshelf = require('bookshelf')(knex);
+
+var User = Bookshelf.Model.extend({
+    tableName: 'users'
+});
+
+router.get('/add', (req, res, next) => {
+    var data = {
+        title: 'User/Add',
+        form: { name: '', password: '', comment: '' },
+        content: '※登録する名前・パスワード・コメントを入力ください。'
+    }
+    res.render('users/add', data);
+});
+router.post('/add', (req, res, next) => {
+    var request = req;
+    var response = res;
+    req.check('name', 'NAME は必ず入力してください').notEmpty();
+    req.check('password', 'PASSWORD は必ず入力してください').notEmpty();
+    req.getValidationResult().then((result) => {
+        if (!result.isEmpty()) {
+            var content = '<ul class="error">';
+            var result_arr = result.array();
+        }
+    })
+})
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
 
 module.exports = router;
